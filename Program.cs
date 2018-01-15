dea9338f27191b034964819bd9d1728e4d181abe
juhanisak <32923453+juhanisak@users.noreply.github.com>
2018-01-15 11:37:07 +0200

    ConsoleApp1 on õige

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programm, mis lubab sisestada N+1 hulga erinevaid sõnu, kuni sisestatakse -1.");
+            Console.WriteLine("------------------------------------------------------");
+
+            Console.WriteLine();
+
+            List<string> sõnad = new List<string>();
+
+            bool tehing = true;
+            
+            while (tehing)
+            {
+                //Console.OutputEncoding = System.Text.Encoding.UTF8;
+                Console.Write("Sisesta sõna: ");
+                string sisend = Console.ReadLine();
+
+                string sisend2 = sisend.ToString();
+
+
+
+                if (sisend2 != "1")
+
+                    sõnad.Add(sisend);
+                    Console.WriteLine();
+
+
+
+                if (sisend2 == "1")
+                {
+                    Console.WriteLine("------------------------------------------------------");
+                    break;
+                }
+
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sisestasid sõnad: ");
+            sõnad.ForEach(Console.WriteLine);
+            Console.WriteLine();
+            Console.WriteLine("Valin nendest välja suvalise: ");
+
+            Random rnd = new Random();
+
+            int indeks = rnd.Next(sõnad.Count);
+
+            string suva_sõna = sõnad[indeks];
+            
+            Console.WriteLine(suva_sõna);
+
+
+            Console.ReadLine();
+
+            }
+        }
+    }
+}
